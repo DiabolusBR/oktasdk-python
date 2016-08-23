5d557461a534a2665dc38793705fbdd781ddfaeb
DiabolusBR <iury_simas@yahoo.com>
2016-08-23 15:56:06 -0400

    Add files via upload

--- okta/models/app/AppInstance.py
+++ okta/models/app/AppInstance.py
@@ -4,71 +4,80 @@ from okta.models.app.Visibility import Visibility
 from okta.models.app.AppCredentials import AppCredentials
 from okta.models.app.Settings import Settings
 from okta.models.app.AppSettings import AppSettings
-
+from okta.models.Link import Link
 
 class AppInstance:
+	types = {
+		'id': str,
+		'name': str,
+		'label': str,
+		'created': datetime,
+		'lastUpdated': datetime,
+		'status': str,
+		'activated': datetime,
+		'features': str,
+		'signOnMode': str,
+		'accessibility': Accessibility,
+		'visibility': Visibility,
+		'credentials': AppCredentials,
+		'settings': Settings
+	}
 
-    types = {
-        'id': str,
-        'name': str,
-        'label': str,
-        'created': datetime,
-        'lastUpdated': datetime,
-        'status': str,
-        'activated': datetime,
-        'features': str,
-        'signOnMode': str,
-        'accessibility': Accessibility,
-        'visibility': Visibility,
-        'credentials': AppCredentials,
-        'settings': Settings
-    }
+	dict_types = {
+		'_links': Link
+	}
 
-    def __init__(self):
+	alt_names = {
+		'_links': 'links'
+	}
 
-        # unique key for app
-        self.id = None  # str
 
-        # unique key for app definition
-        self.name = None  # str
+	def __init__(self):
+		# unique key for app
+		self.id = None  # str
 
-        # unique user-defined display name for app
-        self.label = None  # str
+		# unique key for app definition
+		self.name = None  # str
 
-        # timestamp when app was created
-        self.created = None  # datetime
+		# unique user-defined display name for app
+		self.label = None  # str
 
-        # timestamp when app was last updated
-        self.lastUpdated = None  # datetime
+		# timestamp when app was created
+		self.created = None  # datetime
 
-        # status of app
-        self.status = None  # enum
+		# timestamp when app was last updated
+		self.lastUpdated = None  # datetime
 
-        # timestamp when transition to ACTIVE status completed
-        self.activated = None  # datetime
+		# status of app
+		self.status = None  # enum
 
-        # enabled app features
-        self.features = None  # enum
+		# timestamp when transition to ACTIVE status completed
+		self.activated = None  # datetime
 
-        # authentication mode of app
-        self.signOnMode = None  # enum
+		# enabled app features
+		self.features = None  # enum
 
-        self.accessibility = None  # Accessibility
+		# authentication mode of app
+		self.signOnMode = None  # enum
 
-        self.visibility = None  # Visibility
+		self.accessibility = None  # Accessibility
 
-        self.credentials = None  # AppCredentials
+		self.visibility = None  # Visibility
 
-        self.settings = None  # Settings
+		self.credentials = None  # AppCredentials
 
-    @staticmethod
-    def build_bookmark(url, label=None, request_integration=False):
-        app = AppInstance()
-        app.name = "bookmark"
-        app.label = label
-        app.signOnMode = "BOOKMARK"
-        app.settings = Settings()
-        app.settings.app = AppSettings()
-        app.settings.app.url = url
-        app.settings.app.requestIntegration = request_integration
-        return app
+		self.settings = None  # Settings
+
+		self.links = None
+
+	@staticmethod
+	def build_bookmark(url, label=None, request_integration=False):
+		app = AppInstance()
+		app.name = "bookmark"
+		app.label = label
+		app.signOnMode = "BOOKMARK"
+		app.settings = Settings()
+		app.settings.app = AppSettings()
+		app.settings.app.url = url
+		app.settings.app.requestIntegration = request_integration
+		return app
